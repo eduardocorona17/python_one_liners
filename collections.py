8674ce1f117bc6ac5f993a890aaa90fb7085352f
Eduardo Corona <ecoronalopez@gmail.com>
2023-06-04 21:15:12 -0700

    Just a refresher before you get into one liner algorithms

---
+++ collections.py
@@ -0,0 +1,14 @@
+hero = "Harry"
+guide = "Dumbledore"
+enemy = "Lord V."
+print(hash(hero))
+print(hash(guide))
+
+characters = {hero, guide, enemy}
+print(characters)
+
+team_1 = [hero, guide]
+
+team_2 = [enemy]
+
+# teams = {team_1, team_2
---
+++ the_basics.py
@@ -0,0 +1,21 @@
+# Boolens
+# Can only take two values -- False or True
+# Booleans are internally represented by intergers -- True is 1 & False is 0
+
+print("another".find("other"))
+print('.'.join(["F", "B", "I"]))
+l = [1, 2, 4]
+l.insert(2, 3)
+print(l)
+
+l_2 = [1, 2, 2, 3, 4]
+print(l_2)
+l_2.remove(2)
+print(l_2)
+
+stack = [1]
+stack.append(2)
+stack.append(3)
+print(stack)
+stack.pop()
+print(stack)
